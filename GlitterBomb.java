3986a78b910facb1031693bd47e862695becf80b
MaxwellCross <49821748+MaxwellCross@users.noreply.github.com>
2019-04-23 10:25:23 -0700

    Create GlitterBomb.java

---
+++ GlitterBomb.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import Cities.txt;
+
+int[] x;
+int[] y
+
+public class GlitterBomb {
+
+   public static void main(String[] args) {
+   Scanner input = new Scanner(new File("Cities.txt");
+   int cityCount = input.nextInt();
+   
+   }
+   
+   public static int[] makePoints(int[] x, int[] y) {
+      for (int i = o; i < x.length() + y.length(); i++) {
+           
+      }
+      
+   }
+}
