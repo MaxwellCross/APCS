16f3fe27bc1b1dd31dadc08c95ae68d16897d324
MaxwellCross <49821748+MaxwellCross@users.noreply.github.com>
2019-05-06 12:21:12 -0700

    Update ShoppingTest.java

--- Object_creation/ShoppingTest.java
+++ Object_creation/ShoppingTest.java
@@ -1,21 +1,63 @@
-public class TestShoppingCart {
+public class ShoppingTest {
 
    public static void main(String[] args) {
    
-   Item bubbles = new Item("Bottle of Bubbles", .99);
    Item frisbee = new Item("CS Garfield Frisbee", 2);
    Item bBooks = new Item("CS Book", 2, 4, 5.0);
-   System.out.println(bubbles);
+   
+   //Item frisbeeBad = new Item( null,2, 4, 5);
+   //Item bBooksbad = new Item("CS Book", 2, -5, 5.0);
+   //Item legoBad = new Item("Lego Star Wars Toy", -10);
+   
+   // /*
    System.out.println(frisbee);
    System.out.println(bBooks);
    System.out.println(frisbee.priceFor(5));
-   System.out.println(frisbee.priceFor(1));
-   System.out.println(bubbles.priceFor(5));
-   System.out.println(bBooks.priceFor(5)); // should be 7.00
-   Item bBooksbad = new Item("CS Book", 2, -5, 5.0);
+   System.out.println(bBooks.priceFor(21));
+   // */
+   
+   //System.out.println(frisbeeBad.priceFor(10));
+   //System.out.println(frisbeeBad);
+   //System.out.println(frisbee.priceFor(-1));
+   
+   // /*
+   Catalog springVol = new Catalog("Spring Catalog");
+   System.out.println(springVol.size());
+   springVol.add(frisbee);
+   System.out.println(springVol.size());
+   springVol.add(bBooks);
+   System.out.println(springVol.size());
+   // */ 
 
+
+   ItemOrder itemOrder = new ItemOrder(bBooks, 5);
+   ItemOrder itemOrder1 = new ItemOrder(frisbee, 10);
    
    
+   //Checking ItemoOrder Class Methods
+   /*
+   System.out.println(itemOrder.getPrice());
+   System.out.println(itemOrder.getItem());
+   */
+
+   //Checking ShoppingCart Class Construction
+   /*
+   ShoppingCart shoppingCart = new ShoppingCart();
+   */
+   //Checking ShoppingCart Class Methods
+   
+   /*
+   shoppingCart.add(itemOrder);
+   shoppingCart.add(itemOrder1);
+   shoppingCart.add(itemOrder2);
+   System.out.print(shoppingCart);
+   shoppingCart.setDiscount(true);
+   System.out.println(shoppingCart.getTotal());
+   shoppingCart.setD
+   */
+    
+    
+    
    
    }
 
